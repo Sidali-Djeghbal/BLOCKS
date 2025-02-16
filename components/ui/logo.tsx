7218e4a134de82084a98ffb798e7e378ed916f80
Sidali Djeghbal <sidalidjeghbal@gmail.com>
2025-02-17 00:26:31 +0100

    first commit & initializing project

---
+++ components/ui/logo.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <Link href="/" className="inline-flex shrink-0" aria-label="TechGeeks-logo">
+      <Image
+        src={"/images/logo.svg"}
+        alt="TechGeeks Logo"
+        width={48}
+        height={48}
+      />
+    </Link>
+  );
+}
