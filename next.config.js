22281e84af66ebed8e95a1e5d360b35190ce2131
Sidali Djeghbal <sidalidjeghbal@gmail.com>
2025-02-17 23:47:09 +0100

    updating next config

--- next.config.js
+++ next.config.js
@@ -5,12 +5,4 @@ module.exports = nextConfig;
 
 const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-  output: "export", // This enables static export
-  distDir: "out", // Output directory
-  images: {
-    unoptimized: true, // Required because GitHub Pages doesn't support Next.js image optimization
-  },
-  basePath: isProd ? "/your-repo-name" : "", // Adjust for GitHub Pages
-  assetPrefix: isProd ? "/your-repo-name/" : "", // Adjust for GitHub Pages
-};
+module.exports = {};
