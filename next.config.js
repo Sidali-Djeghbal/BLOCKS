3acc11b9b53d50f59ba97bded2d31093f2ca8303
Sidali Djeghbal <sidalidjeghbal@gmail.com>
2025-02-21 16:09:17 +0100

    fixing webpack compatiblity

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: true,
+  experimental: {
+    optimizeCss: true
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false
+      };
+    }
+    return config;
+  }
+};
 
 module.exports = nextConfig;
-
-const isProd = process.env.NODE_ENV === "production";
 
